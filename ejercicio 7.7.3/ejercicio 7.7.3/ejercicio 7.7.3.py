fc2dae5b4ad79c5b1bab1ce467f3efa61612e6f7
alvarolalomiaa <alvarolalomia07@gmail.com>
2025-10-03 21:27:55 -0300

    Update ejercicio 7.7.3.py

---
+++ ejercicio 7.7.3/ejercicio 7.7.3/ejercicio 7.7.3.py
@@ -0,0 +1,10 @@
+#a)
+def votar(tupla_nombres): 
+    for i in tupla_nombres: 
+        print("Estimado", i , ", vote por mi")
+    
+
+tupla1= ('alvaro', 'santiago', 'chamorro') 
+    
+    
+votar(tupla1)
